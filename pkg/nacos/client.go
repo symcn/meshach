662abdc93a9a4596d4da68f2a23503a2d554105c
yangyongzhi7 <sweet.yangyz@qq.com>
2020-05-12 13:24:43 +0800

    Optimizing the client of Nacos.

--- pkg/nacos/client.go
+++ pkg/nacos/client.go
@@ -1,21 +1,20 @@
 package nacos
 
 import (
-	"github.com/nacos-group/nacos-sdk-go/clients/nacos_client"
+	"github.com/nacos-group/nacos-sdk-go/clients"
 	"github.com/nacos-group/nacos-sdk-go/clients/naming_client"
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
-	"github.com/nacos-group/nacos-sdk-go/common/http_agent"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sync"
 )
 
 var (
-	nClient *naming_client.NamingClient
+	nClient naming_client.INamingClient
 	lock    sync.Mutex
 )
 var log = logf.Log.WithName("nacos")
 
-func NewClient() (*naming_client.NamingClient, error) {
+func NewClient() (naming_client.INamingClient, error) {
 	var err error
 	lock.Lock()
 	defer lock.Unlock()
@@ -23,25 +22,23 @@ func NewClient() (*naming_client.NamingClient, error) {
 	if nClient == nil {
 		log.Info("Starting create a new nacos client.")
 
-		clientConfig := constant.ClientConfig{
-			TimeoutMs:           10 * 1000,
-			BeatInterval:        5 * 1000,
-			ListenInterval:      30 * 1000,
-			NotLoadCacheAtStart: true,
-		}
+		nc, e := clients.CreateNamingClient(map[string]interface{}{
+			"serverConfigs": []constant.ServerConfig{
+				{
+					IpAddr:      "10.248.224.144",
+					Port:        8848,
+					ContextPath: "/nacos",
+				},
+			},
+			"clientConfig": constant.ClientConfig{
+				TimeoutMs:           10 * 1000,
+				BeatInterval:        5 * 1000,
+				ListenInterval:      30 * 1000,
+				NotLoadCacheAtStart: true,
+			},
+		})
 
-		var serverConfig = constant.ServerConfig{
-			IpAddr:      "10.248.224.144",
-			Port:        8848,
-			ContextPath: "/nacos",
-		}
-
-		nc := nacos_client.NacosClient{}
-		nc.SetServerConfig([]constant.ServerConfig{serverConfig})
-		nc.SetClientConfig(clientConfig)
-		nc.SetHttpAgent(&http_agent.HttpAgent{})
-		c, e := naming_client.NewNamingClient(&nc)
-		nClient = &c
+		nClient = nc
 		err = e
 	}
 	return nClient, err
