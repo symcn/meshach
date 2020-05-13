52bd06161e1aa515e7fd0cad8ec9e6129c2b3bda
yangyongzhi7 <sweet.yangyz@qq.com>
2020-05-13 13:43:06 +0800

    Generate the empty apis and controllers.

---
+++ pkg/controller/add_appmeshconfig.go
@@ -0,0 +1,10 @@
+package controller
+
+import (
+	"github.com/mesh-operator/pkg/controller/appmeshconfig"
+)
+
+func init() {
+	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
+	AddToManagerFuncs = append(AddToManagerFuncs, appmeshconfig.Add)
+}
