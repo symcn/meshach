2d08d361745f257ba82150e7bcebb4c03b90e6fa
张海东 <haidongdev@gmail.com>
2020-05-19 16:28:54 +0800

    update AppMeshConfig CRD

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.13
 require (
 	github.com/nacos-group/nacos-sdk-go v0.3.2
 	github.com/operator-framework/operator-sdk v0.17.0
+	github.com/pkg/errors v0.9.1
+	github.com/samuel/go-zookeeper v0.0.0-20190923202752-2cc03de413da
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.5.1
 	google.golang.org/grpc v1.29.1
--- pkg/apis/mesh/v1/appmeshconfig_types.go
+++ pkg/apis/mesh/v1/appmeshconfig_types.go
@@ -1,24 +1,88 @@
 package v1
 
 import (
+	networking "istio.io/api/networking/v1alpha3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-// AppMeshConfigSpec defines the desired state of AppMeshConfig
+// Inject ...
+type Inject struct {
+	LogLevel string `json:"logLevel,omitempty"`
+	Sidecar  string `json:"sidecar"`
+}
+
+// Port ...
+type Port struct {
+	Name     string `json:"name"`
+	Protocol string `json:"protocol"`
+	Port     int32  `json:"port"`
+}
+
+// Instance ...
+type Instance struct {
+	Host       string            `json:"host"`
+	Zone       string            `json:"zone"`
+	Group      string            `json:"group"`
+	Labels     map[string]string `json:"labels,omitempty"`
+	Dynamic    bool              `json:"dynamic,omitempty"`
+	Weight     string            `json:"weight,omitempty"`
+	Methods    []string          `json:"methods,omitempty"`
+	ProCode    string            `json:"pro_code,omitempty"`
+	Release    string            `json:"release,omitempty"`
+	SdkVersion string            `json:"sdk_version,omitempty"`
+	Side       string            `json:"side,omitempty"`
+	Timeout    int32             `json:"timeout,omitempty"`
+	Retry      int32             `json:"retry,omitempty"`
+	Timestamp  int64             `json:"timestamp,omitempty"`
+}
+
+// Service ...
+type Service struct {
+	Name        string      `json:"name"`
+	Application string      `json:"application,omitempty"`
+	Ports       []*Port     `json:"ports"`
+	Instances   []*Instance `json:"instances"`
+}
+
+// Policy ...
+type Policy struct {
+	LoadBalancer      *networking.LoadBalancerSettings `json:"load_balancer,omitempty"`
+	MaxConnections    int32                            `json:"max_connections,omitempty"`
+	ConnectionTimeout string                           `json:"connection_timeout,omitempty"`
+	MaxRetries        int32                            `json:"max_retries,omitempty"`
+}
+
+// Monitor ...
+// type Monitor struct {
+// }
+
+// AppMeshConfigSpec ...
 type AppMeshConfigSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Inject   *Inject    `json:"inject"`
+	Services []*Service `json:"services"`
+	Policy   *Policy    `json:"policy"`
 }
+
+type ConfigStatus string
+
+const (
+	ConfigStatusUndistributed ConfigStatus = "Undistributed"
+	ConfigStatusDistributed   ConfigStatus = "Distributed"
+	ConfigStatusUpdating      ConfigStatus = "Updating"
+	ConfigStatusUnknown       ConfigStatus = "Unknown"
+)
 
 // AppMeshConfigStatus defines the observed state of AppMeshConfig
 type AppMeshConfigStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	LastUpdateTime *metav1.Time `json:"lastUpdateTime,omitempty"`
+	Status         ConfigStatus
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
