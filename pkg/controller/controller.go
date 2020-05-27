eeea6ef522ef6310d686355d29ab9c83b9088184
haidong <haidongdev@gmail.com>
2020-05-27 11:22:44 +0800

    refactoring the controller registration logic

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -2,6 +2,8 @@
 package controller
 
 import (
+	"github.com/mesh-operator/pkg/controller/appmeshconfig"
+	"github.com/mesh-operator/pkg/controller/istioconfig"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
@@ -10,6 +12,9 @@ var AddToManagerFuncs []func(manager.Manager) error
 
 // AddToManager adds all Controllers to the Manager
 func AddToManager(m manager.Manager) error {
+	AddToManagerFuncs = append(AddToManagerFuncs, appmeshconfig.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, istioconfig.Add)
+
 	for _, f := range AddToManagerFuncs {
 		if err := f(m); err != nil {
 			return err
