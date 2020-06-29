a11d5836800be1b72ea985ea88d78f9dea681dab
champly <champly@outlook.com>
2020-06-29 08:34:15 +0800

    fix package repeat

--- pkg/adapter/zookeeper/registry_client.go
+++ pkg/adapter/zookeeper/registry_client.go
@@ -2,8 +2,6 @@ package zookeeper
 
 import (
 	"fmt"
-	"github.com/mesh-operator/pkg/adapter/constant"
-	"k8s.io/klog"
 	"net/url"
 	"path"
 	"strings"
@@ -12,6 +10,7 @@ import (
 	"github.com/mesh-operator/pkg/adapter/constant"
 	"github.com/mesh-operator/pkg/adapter/events"
 	"github.com/samuel/go-zookeeper/zk"
+	"k8s.io/klog"
 )
 
 type ZkRegistryClient struct {
