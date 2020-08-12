d3dc77800500c85fac4178dd5cb50437e71a8c0c
haidong <haidongdev@gmail.com>
2020-08-12 11:28:21 +0800

    test: add unit testcases for update status and workloadentry

--- controllers/serviceconfig/status_test.go
+++ controllers/serviceconfig/status_test.go
@@ -12,7 +12,6 @@ import (
 	v1beta1 "istio.io/api/networking/v1beta1"
 	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -20,14 +19,17 @@ import (
 
 var _ = Describe("test status update", func() {
 	var (
-		mockCtrl         *gomock.Controller
-		mockClient       *MockClient
-		req              ctrl.Request
-		sc               *meshv1alpha1.ServiceConfig
-		cs               *meshv1alpha1.ConfiguredService
-		vs               *networkingv1beta1.VirtualService
-		normalRoute      []*meshv1alpha1.Destination
-		mockStatusWriter *MockStatusWriter
+		mockCtrl          *gomock.Controller
+		mockClient        *MockClient
+		req               ctrl.Request
+		sc                *meshv1alpha1.ServiceConfig
+		cs                *meshv1alpha1.ConfiguredService
+		vs                *networkingv1beta1.VirtualService
+		vsList            *networkingv1beta1.VirtualServiceList
+		dr                *networkingv1beta1.DestinationRule
+		normalRoute       []*meshv1alpha1.Destination
+		mockStatusWriter  *MockStatusWriter
+		zero, one, second int
 	)
 
 	BeforeEach(func() {
@@ -133,6 +135,24 @@ var _ = Describe("test status update", func() {
 				Http:  nil,
 			},
 		}
+
+		dr = &networkingv1beta1.DestinationRule{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "normal.reconcile.serviceconfig",
+				Namespace: "mesh-test",
+				Labels: map[string]string{
+					"service": "Normal.Reconcile.ServiceConfig",
+				},
+			},
+			Spec: v1beta1.DestinationRule{
+				Host: "normal.reconcile.serviceconfig",
+			},
+		}
+
+		zero = 0
+		one = 1
+		second = 2
+
 	}, timeout)
 
 	Describe("test get error when after update status", func() {
@@ -163,10 +183,16 @@ var _ = Describe("test status update", func() {
 	})
 
 	Describe("test get virtualservice status", func() {
+		BeforeEach(func() {
+			mockCtrl = gomock.NewController(GinkgoT())
+			mockClient = NewMockClient(mockCtrl)
+			mockClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("get cs error"))
+		}, timeout)
+
 		It("occurred error when get subset, return default status", func() {
 			r := Reconciler{
-				Client:     getFakeClient(getTestMeshConfig()),
-				Scheme:     &runtime.Scheme{},
+				Client:     mockClient,
+				Scheme:     getFakeScheme(),
 				Log:        nil,
 				Opt:        testOpt,
 				MeshConfig: getTestMeshConfig(),
@@ -179,7 +205,9 @@ var _ = Describe("test status update", func() {
 				Undistributed: &svcCount,
 			}))
 		})
+	})
 
+	Describe("test get virtualservice status", func() {
 		It("return right virtualservice status", func() {
 			r := Reconciler{
 				Client:     getFakeClient(getTestMeshConfig(), sc, cs, vs),
@@ -196,6 +224,284 @@ var _ = Describe("test status update", func() {
 				Distributed:   &svcCount,
 				Undistributed: &undistributed,
 			}))
+		})
+	})
+
+	Describe("test get destinationrule status with mock client", func() {
+		BeforeEach(func() {
+			mockCtrl = gomock.NewController(GinkgoT())
+			mockClient = NewMockClient(mockCtrl)
+			mockClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("get cs error"))
+		}, timeout)
+
+		It("occurred error when get subset, return default status", func() {
+			r := Reconciler{
+				Client:     mockClient,
+				Scheme:     getFakeScheme(),
+				Log:        nil,
+				Opt:        testOpt,
+				MeshConfig: getTestMeshConfig(),
+			}
+			result := r.getDestinationRuleStatus(context.Background(), sc)
+			svcCount := 0
+			Expect(result).To(Equal(&meshv1alpha1.SubStatus{
+				Desired:       svcCount,
+				Distributed:   &svcCount,
+				Undistributed: &svcCount,
+			}))
+		})
+	})
+
+	Describe("test get virtualservice status", func() {
+		It("return right virtualservice status", func() {
+			r := Reconciler{
+				Client:     getFakeClient(getTestMeshConfig(), sc, cs, dr),
+				Scheme:     getFakeScheme(),
+				Log:        nil,
+				Opt:        testOpt,
+				MeshConfig: getTestMeshConfig(),
+			}
+			result := r.getDestinationRuleStatus(context.Background(), sc)
+			svcCount := 1
+			undistributed := 0
+			Expect(result).To(Equal(&meshv1alpha1.SubStatus{
+				Desired:       svcCount,
+				Distributed:   &svcCount,
+				Undistributed: &undistributed,
+			}))
+		})
+	})
+
+	Describe("test count with mock client", func() {
+		BeforeEach(func() {
+			mockCtrl = gomock.NewController(GinkgoT())
+			mockClient = NewMockClient(mockCtrl)
+			vsList = &networkingv1beta1.VirtualServiceList{}
+			mockClient.EXPECT().List(gomock.Any(), gomock.Eq(vsList), gomock.Any()).Return(errors.New("list vs error"))
+			mockClient.EXPECT().List(gomock.Any(), gomock.Not(vsList), gomock.Any()).Return(nil)
+		}, timeout)
+
+		It("occurred error when list vs, return nil", func() {
+			r := Reconciler{
+				Client:     mockClient,
+				Scheme:     getFakeScheme(),
+				Log:        nil,
+				Opt:        testOpt,
+				MeshConfig: getTestMeshConfig(),
+			}
+			result := r.count(context.Background(), sc, vsList)
+			var want *int
+			want = nil
+			Expect(result).To(Equal(want))
+		})
+
+		It("occurred error invalid list type", func() {
+			r := Reconciler{
+				Client:     mockClient,
+				Scheme:     getFakeScheme(),
+				Log:        nil,
+				Opt:        testOpt,
+				MeshConfig: getTestMeshConfig(),
+			}
+			invalidList := &meshv1alpha1.MeshConfigList{}
+			result := r.count(context.Background(), sc, invalidList)
+			var want int = 0
+			Expect(result).To(Equal(&want))
+		})
+	})
+
+	Describe("test calculate phase", func() {
+		Context("return Unknown if any distributed is nil", func() {
+			It("workloadentry distributed is nil", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   nil,
+						Undistributed: &second,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &zero,
+						Undistributed: &one,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &one,
+						Undistributed: &one,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusUnknown))
+			}, timeout)
+			It("virtualservice distributed is nil", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   nil,
+						Undistributed: &one,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &one,
+						Undistributed: &one,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusUnknown))
+			}, timeout)
+			It("serviceentry distributed is nil", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   nil,
+						Undistributed: &second,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &zero,
+						Undistributed: &one,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &one,
+						Undistributed: &one,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusUnknown))
+			}, timeout)
+			It("destinationrule distributed is nil", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &zero,
+						Undistributed: &one,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   nil,
+						Undistributed: &second,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusUnknown))
+			}, timeout)
+		})
+		Context("return Undistributed if the sum of all Distributed is zero", func() {
+			It("all distributed is zero", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &zero,
+						Undistributed: &one,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &zero,
+						Undistributed: &second,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusUndistributed))
+			}, timeout)
+		})
+
+		Context("return Distributed if the sum of all Undistributed is zero", func() {
+			It("all undistributed is zero", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &second,
+						Undistributed: &zero,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &second,
+						Undistributed: &zero,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &one,
+						Undistributed: &zero,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &second,
+						Undistributed: &zero,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusDistributed))
+			}, timeout)
+		})
+
+		Context("return Distributing", func() {
+			It("success", func() {
+				status := &meshv1alpha1.Status{
+					ServiceEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &one,
+						Undistributed: &one,
+					},
+					WorkloadEntry: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &second,
+						Undistributed: &zero,
+					},
+					VirtualService: &meshv1alpha1.SubStatus{
+						Desired:       1,
+						Distributed:   &one,
+						Undistributed: &zero,
+					},
+					DestinationRule: &meshv1alpha1.SubStatus{
+						Desired:       2,
+						Distributed:   &second,
+						Undistributed: &zero,
+					},
+				}
+				result := calcPhase(status)
+				Expect(result).To(Equal(meshv1alpha1.ConfigStatusDistributing))
+			}, timeout)
 		})
 	})
 })
--- controllers/serviceconfig/workloadentry_test.go
+++ controllers/serviceconfig/workloadentry_test.go
@@ -9,7 +9,10 @@ import (
 	. "github.com/onsi/gomega"
 	meshv1alpha1 "github.com/symcn/mesh-operator/api/v1alpha1"
 	. "github.com/symcn/mesh-operator/test"
+	v1beta1 "istio.io/api/networking/v1beta1"
+	networkingv1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 var _ = Describe("test workloadentry update", func() {
@@ -18,6 +21,7 @@ var _ = Describe("test workloadentry update", func() {
 		mockClient  *MockClient
 		sc          *meshv1alpha1.ServiceConfig
 		normalRoute []*meshv1alpha1.Destination
+		we          *networkingv1beta1.WorkloadEntry
 	)
 
 	BeforeEach(func() {
@@ -43,12 +47,14 @@ var _ = Describe("test workloadentry update", func() {
 				Route: normalRoute,
 				Instances: []*meshv1alpha1.InstanceConfig{
 					{
-						Host: "1.1.1.1",
-						Port: &meshv1alpha1.Port{Name: "test-port", Number: 12345, Protocol: "HTTP"},
+						Host:   "1.1.1.1",
+						Weight: 80,
+						Port:   &meshv1alpha1.Port{Name: "test-port", Number: 12345, Protocol: "HTTP"},
 					},
 					{
-						Host: "1.1.1.2",
-						Port: &meshv1alpha1.Port{Name: "test-port", Number: 12345, Protocol: "HTTP"},
+						Host:   "1.1.1.2",
+						Weight: 20,
+						Port:   &meshv1alpha1.Port{Name: "test-port", Number: 12345, Protocol: "HTTP"},
 					},
 				},
 				RerouteOption: &meshv1alpha1.RerouteOption{
@@ -58,6 +64,19 @@ var _ = Describe("test workloadentry update", func() {
 					ReroutePolicy: meshv1alpha1.Default,
 				},
 				MeshConfigGeneration: 0,
+			},
+		}
+		we = &networkingv1beta1.WorkloadEntry{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "test.workloadentry.com.1.1.1.1.12345",
+				Namespace: "mesh-test",
+			},
+			Spec: v1beta1.WorkloadEntry{
+				Address: "1.1.1.1",
+				Weight:  20,
+				Ports: map[string]uint32{
+					"test-port": 12345,
+				},
 			},
 		}
 	}, timeout)
@@ -88,6 +107,7 @@ var _ = Describe("test workloadentry update", func() {
 			Expect(err.Error()).To(Equal("get workloadentry error"))
 		}, timeout)
 	})
+
 	Describe("occured update error", func() {
 		BeforeEach(func() {
 			mockCtrl = gomock.NewController(GinkgoT())
@@ -113,6 +133,27 @@ var _ = Describe("test workloadentry update", func() {
 			err := r.reconcileWorkloadEntry(context.Background(), sc)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("update workloadentry error"))
+		}, timeout)
+	})
+
+	Describe("update success", func() {
+		It("return nil", func() {
+			r := Reconciler{
+				Client:     getFakeClient(getTestMeshConfig(), sc, we),
+				Scheme:     getFakeScheme(),
+				Log:        nil,
+				Opt:        testOpt,
+				MeshConfig: getTestMeshConfig(),
+			}
+
+			err := r.reconcileWorkloadEntry(context.Background(), sc)
+			Expect(err).NotTo(HaveOccurred())
+
+			found := &networkingv1beta1.WorkloadEntry{}
+			name := "test.workloadentry.com.1.1.1.1.12345"
+			err = r.Get(context.Background(), types.NamespacedName{Namespace: "mesh-test", Name: name}, found)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(found.Spec.Weight).To(Equal(uint32(80)))
 		}, timeout)
 	})
 })
